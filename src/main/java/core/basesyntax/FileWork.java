91eb12b791eadfe1cb8c55e65ca230f764a666f2
Nataliia Batiukova <batiukova.nataliia@gmail.com>
2022-09-30 18:26:22 +0100

    The file was read, but an empty array is returned.

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,40 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.WeakHashMap;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+       File file = new File(fileName);
+       String[] arrayResult;
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+            StringBuilder builder = new StringBuilder();
+            String value = bufferedReader.readLine();
+            if (value == null) {
+                return new String[0];
+            }
+            while (value != null) {
+String sentence = value.toLowerCase();
+String[] split = sentence.split("\\W+");
+for (String words: split) {
+    if (words.charAt(0) =='w') {
+        builder.append(words).append(" ");
+    }
+return new String[0];
+
+}
+value = bufferedReader.readLine();
+            }
+            arrayResult = builder.toString().split(" ");
+            Arrays.sort(arrayResult);
+        }catch (IOException e) {
+            throw new RuntimeException("Can't read a file", e);
+        }
+return arrayResult;
     }
 }
